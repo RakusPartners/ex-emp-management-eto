786c770136480b8105c634af20462931112f3052
Takao_Etogit config --global user.email takaoetoooo.@gmail.comgit config --listgit config --listfpath=(~/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.8.1/functions)zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bashautoload -Uz compinit && compinitgit config --global user.name Takao_Eto <takaoetoooo@gmail.com>
2024-05-14 11:39:01 +0900

    1番終了

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -2,9 +2,11 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
 @Controller
@@ -15,7 +17,8 @@ public class AdministratorController {
 private AdministratorService administratorService;
 
 @GetMapping("/toInsert")
-public String toInsert(){
+public String toInsert(InsertAdministratorForm form, Model model){
+    model.addAttribute("form", form);
     return "administrator/insert.html";
 }    
 }
