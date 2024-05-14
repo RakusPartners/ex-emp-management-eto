c4969853fb4d74745f18202e96c244dcf0ad1e3d
Takao_Etogit config --global user.email takaoetoooo.@gmail.comgit config --listgit config --listfpath=(~/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /Users/etotakao/.zsh /usr/local/share/zsh/site-functions /usr/share/zsh/site-functions /usr/share/zsh/5.8.1/functions)zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bashautoload -Uz compinit && compinitgit config --global user.name Takao_Eto <takaoetoooo@gmail.com>
2024-05-14 13:03:37 +0900

    AdministratorController クラスにメソッドの追加

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
@@ -21,4 +23,21 @@ public String toInsert(InsertAdministratorForm form, Model model){
     model.addAttribute("form", form);
     return "administrator/insert.html";
 }    
+
+
+@PostMapping("/insert")
+public String insert(InsertAdministratorForm form){
+    Administrator administrator = new Administrator();
+    
+    administrator.setName(form.getName());
+    administrator.setMailAddress(form.getMailAddress());
+    administrator.setPassword(form.getPassword());
+
+    administratorService.insert(administrator);
+
+    // 「/」(ログイン画面)にリダイレクトする
+    return "redirect:/login";
+
+}
+
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -3,7 +3,9 @@ package com.example.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -13,4 +15,8 @@ public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository ;
 
+    //管理者情報の挿入
+    public void insert(Administrator administrator){
+
+    }
 }
